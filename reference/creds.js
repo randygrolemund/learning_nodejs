026223d893443a6e54dcbbb2406ab5ca1b509902
Randy Grolemund <randygrolemund@Randys-MacBook-Pro.local>
2019-12-13 07:53:09 -0600

    added alert test

--- reference/creds.js
+++ reference/creds.js
@@ -1,10 +1,11 @@
+const alert = require('alert-node')
 const prompt = require('prompt');
 
 const properties = [
   {
     name: 'username',
-    validator: /^[a-zA-Z\s\-]+$/,
-    warning: 'Username must be only letters, spaces, or dashes',
+    validator: /^[a-zA-Z\-]+$/,
+    warning: 'Username must be only letters or dashes',
   },
   {
     name: 'password',
@@ -16,12 +17,17 @@ prompt.start();
 
 prompt.get(properties, function(err, result) {
   if (err) {
-return onErr(err);
-}
-  console.log('Command-line input received:');
-  console.log('  Username: ' + result.username);
-  console.log('  Password: ' + result.password);
+    return onErr(err);
+  }
+  if (result.username == '') {
+    alert('Username cannot be null!');
+  } else {
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Password: ' + result.password);
+  };
 });
+
 
 function onErr(err) {
   console.log(err);
